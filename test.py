587de6ac34b28606bed933900cda166518b3db83
Yi-Hsuan Fu <yhf211@crunchy5.cims.nyu.edu>
2019-02-26 10:43:55 -0500

    added timer tests

--- test.py
+++ test.py
@@ -4,12 +4,34 @@ import time
 import pandas as pd
 from p2k import *
 import numpy
+import timeit
 
-#start = time.time()
 opsd_daily = pd.read_csv('opsd_germany_daily.csv')
 #data_columns = ['Consumption', 'Wind', 'Solar', 'Wind+Solar']
-opsd_7d = opsd_daily['Consumption'].rolling(7, min_periods=1).mean()
+
+## Creating function so it can be timed in timeit 
+## Timing pandas rolling mean
+def pandas_rolling_mean(): 
+	opsd_7d = opsd_daily['Consumption'].rolling(7, min_periods=1).mean()
+
+duration = timeit.timeit(pandas_rolling_mean, number=1000)
+print("Time in pandas: " + str(duration/1000))
 
 k_opsd_7d = kDataFrame(opsd_daily['Consumption'])
-print(k_opsd_7d.rolling_mean(7))
-#print(opsd_7d == numpy.asarray(k_opsd_7d.rolling_mean(7))) 
+
+## Timing k7 rolling mean
+def k7_rolling_mean():
+	k_opsd_7d.rolling_mean(7)
+
+duration = timeit.timeit(k7_rolling_mean, number=1000)
+print("Time in k7: " + str(duration/1000))
+
+
+## Calculate difference in results
+def percent_diff(a, b):
+	diff = (b - a) / a
+	print("percent difference in calculations:"+str(percent))
+
+## Moved your commented lines down
+#print(k_opsd_7d.rolling_mean(7))
+#print(opsd_7d == numpy.asarray(k_opsd_7d.rolling_mean(7)))
