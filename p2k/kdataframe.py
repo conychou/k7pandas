dfbcc477a38f1379d67966ed5be48999a3aedf2e
Ruby Fu <ruby.yh.fu@gmail.com>
2019-03-11 15:52:04 -0400

    fixed space tab inconsistency

--- p2k/kdataframe.py
+++ p2k/kdataframe.py
@@ -14,17 +14,17 @@ class kDataFrame(object):
         #should be able to accept different data : numpy ndarray (structured or homogeneous), dict, or DataFrame
         if isinstance(data,numpy.ndarray):
             self.kcopy = k('[]')
-	    self.kcopy.x = data
+            self.kcopy.x = data
             #numpy.asarray(self.kcopy)[:] = data
         else:
             if isinstance(data, list):
                 #self.kcopy = shape(len(data),0.0)
-            	self.kcopy = k('[]')
-	    else:
+                self.kcopy = k('[]')
+            else:
                 self.kcopy = k('[]')
             tmp = numpy.asarray(data)
             #numpy.asarray(self.kcopy)[:] = tmp
-            self.kcopy.x = temp
+            self.kcopy.x = tmp
         return        
 
     def append(self, other, ignore_index=False, verify_integrity=False, sort=None):
@@ -34,7 +34,7 @@ class kDataFrame(object):
         return k('(+/{0})%(#{0})'.format(self.kcopy))
 
     def rolling_mean(self, numpoints):
-	print(self.k)
+        #print(self.kcopy)
         return k('movavg[{1};{0}]'.format(self.kcopy.x, numpoints))
 
     def to_ndarray(self):
