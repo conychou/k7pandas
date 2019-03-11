02b55cba2056f8c306fec6d5afb79acc06346fda
Ruby Fu <ruby.yh.fu@gmail.com>
2019-03-11 15:22:54 -0400

    changed k array object initialization

--- p2k/kdataframe.py
+++ p2k/kdataframe.py
@@ -13,16 +13,18 @@ class kDataFrame(object):
     ):
         #should be able to accept different data : numpy ndarray (structured or homogeneous), dict, or DataFrame
         if isinstance(data,numpy.ndarray):
-            self.kcopy = shape(data.size,0.0)
-            numpy.asarray(self.kcopy)[:] = data
+            self.kcopy = k('[]')
+	    self.kcopy.x = data
+            #numpy.asarray(self.kcopy)[:] = data
         else:
             if isinstance(data, list):
-                self.kcopy = shape(len(data),0.0)
-            else:
-                self.kcopy = shape(data.size,0.0)
+                #self.kcopy = shape(len(data),0.0)
+            	self.kcopy = k('[]')
+	    else:
+                self.kcopy = k('[]')
             tmp = numpy.asarray(data)
-            numpy.asarray(self.kcopy)[:] = tmp
-
+            #numpy.asarray(self.kcopy)[:] = tmp
+            self.kcopy.x = temp
         return        
 
     def append(self, other, ignore_index=False, verify_integrity=False, sort=None):
@@ -32,7 +34,8 @@ class kDataFrame(object):
         return k('(+/{0})%(#{0})'.format(self.kcopy))
 
     def rolling_mean(self, numpoints):
-        return k('movavg[{1};{0}]'.format(self.kcopy, numpoints))
+	print(self.k)
+        return k('movavg[{1};{0}]'.format(self.kcopy.x, numpoints))
 
     def to_ndarray(self):
         return numpy.asarray(self.kcopy)
