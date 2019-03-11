5fb17b4942b3ae977a507982374cfe1bcd69ae7f
Ruby Fu <ruby.yh.fu@gmail.com>
2019-03-11 17:06:55 -0400

    fixed updated k7 rolling mean syntax

--- p2k/__init__.py
+++ p2k/__init__.py
@@ -1,6 +1,6 @@
 from k7 import k
 
-k('movsum:{[k;myarray] +\/\'myarray[((!((#myarray)-k))+\:!k)]}')
+k('movsum:{[k;myarray] +/\'myarray[((!((#myarray)-k))+\:!k)]}')
 #k('movsum:{[numpoints;myarray] x: +\myarray; xsub: $[numpoints < #myarray;(numpoints # 0),x[!(#x)-numpoints];0]; x - xsub}')
 k('movavg:{[numpoints;myarray] x: movsum[numpoints; myarray]; mydivs: $[numpoints < #myarray; (1+!numpoints), numpoints _ (#myarray) # numpoints;(1+!#myarray)]; x%mydivs}')
 
